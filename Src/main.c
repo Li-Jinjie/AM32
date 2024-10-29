709519acc1b5edb5a7f88111e8e5813b39353611
LI, Jinjie <45286479+Li-Jinjie@users.noreply.github.com>
2024-10-29 16:51:37 +0900

    Update main.c

--- Src/main.c
+++ Src/main.c
@@ -267,7 +267,7 @@ void zcfoundroutine(void);
 //=============================  Defaults =============================
 //===========================================================================
 
-uint8_t drive_by_rpm = 0;
+uint8_t drive_by_rpm = 1;
 uint32_t MAXIMUM_RPM_SPEED_CONTROL = 10000;
 uint32_t MINIMUM_RPM_SPEED_CONTROL = 1000;
 
